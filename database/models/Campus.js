d0f01432797df08ec76047401213b5d05a400e84
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-07 23:39:33 -0400

    Campus model added imageURL and address properties, reflected in seedDB

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -5,7 +5,22 @@ const Campus = db.define("campus", {
 
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    notEmpty: true
+  },
+
+  imageURL: {
+    type: Sequelize.STRING,
+    defaultValue: "https://cdn.onlinewebfonts.com/svg/img_379742.png",
+    validate: {
+      isURL: true
+    }
+  },
+
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true
   },
 
   description: {
--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,10 +3,14 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
+		address: "695 Park Avenue, New York, NY 10065",
+		imageURL: "http://micefa.org/wp-content/uploads/2011/10/005-1024x768.jpg",
 		description: "This is a school in NYC."
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		address: "Massachusetts Hall, Cambridge, MA 02138",
+		imageURL: "http://atlantablackstar.com/wp-content/uploads/2014/05/Harvard.jpg",
 		description: "This is a school in MA."
 	});
 
